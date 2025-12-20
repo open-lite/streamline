5b3d207f0d654c6fcb72e6873dd6e093834fc04a
Artin Alavi <1361714+Arastais@users.noreply.github.com>
2025-12-20 09:22:21 -0800

    Add placeholder rich result type

---
+++ include/streamline/diagnostics/result.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace sl {
+	//TODO
+	//template<typename T>
+	//using result = OUTCOME_V2_NAMESPACE::experimental::status_result<T>;
+
+	template<typename T>
+	using result = T;
+}
