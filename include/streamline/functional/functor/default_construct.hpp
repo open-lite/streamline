5fc7d9347f903c5c430c68a9c351b13ae0669ab6
Artin Alavi <1361714+Arastais@users.noreply.github.com>
2026-02-19 09:18:54 -0800

    Add functor for default construction

---
+++ include/streamline/functional/functor/default_construct.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "streamline/metaprogramming/integral_constant.hpp"
+
+namespace sl::functor {
+	template<typename T>
+	struct default_construct {
+		template<typename Arg, index_t I = npos>
+		constexpr T operator()(Arg&&, index_constant_type<I> = {}) const noexcept {
+			return T{};
+		};
+	};
+}
